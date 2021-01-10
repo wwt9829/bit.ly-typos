a83e99f43279fe7c2893f062f6d05060b46249b5
Wyatt Tauber <wwt9829@g.rit.edu>
2021-01-10 14:36:02 -0600

    add ability to create a custom bitlink

--- cvs-url-shortener.py
+++ cvs-url-shortener.py
@@ -1,20 +1,60 @@
+from urllib.parse import urlparse
+import json
 import requests
 import sys
 
+# TODO: comments
+# TODO: make a headers class
 
-def create_link(head):
-    data = '{ "long_url": "https://example.com/?1", "domain": "bit.ly" }'
+
+def create_link(head, long_url):
+    """
+    Shortens a long URL into a bit.ly short link
+    :param head: request headers (API key and content type)
+    :param long_url: a properly-formatted URL (TODO: implement a check for this)
+    :return: a properly-formatted bit.ly short link URL
+    """
+    p_data = {
+        'long_url': long_url,
+        'domain': 'bit.ly'
+    }
+    data = json.dumps(p_data)
+
+    # TODO: try/except for json dump
 
     response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=head, data=data)
-    print(response)
+    re_json = json.loads(response.content)
+    try:
+        short_url = re_json["link"]
+        print(response)
+        return short_url
+    except KeyError:
+        print("Error reading JSON")
+        exit()
+
+    # TODO: implement handling of 200/201 and error messages
 
 
-def update_custom(head):
+def update_custom(head, old_link, new_link):
+    """
+    Changes the short link of a bit.ly URL
+    :param head: request headers (API key and content type)
+    :param old_link: a bitlink ID to change
+    :param new_link: an unused bitlink ID
+    :return: HTTP status code of request (TODO: add succes/fail messages instead)
+    """
+    data = {
+        'custom_bitlink': new_link,
+        'bitlink_id': old_link
+    }
+    data = json.dumps(data)
 
-    data = '{ "custom_bitlink": "bit.ly/testingifthelinkisworking1102021", "bitlink_id": "bit.ly/3bn3VmV" }'
+    # TODO: try/except for json dump
 
     response = requests.post('https://api-ssl.bitly.com/v4/custom_bitlinks', headers=head, data=data)
-    print(response)
+    return response
+
+    # TODO: implement handling of 200 and error messages
 
 
 if __name__ == "__main__":
@@ -28,5 +68,12 @@ if __name__ == "__main__":
         'Content-Type': 'application/json',
     }
 
-    #create_link(headers)
-    update_custom(headers)
+    url_to_shorten = 'https://example.net/'
+    bitly_link = create_link(headers, url_to_shorten)
+    parsed = urlparse(bitly_link)
+    bitly_link = parsed.netloc + parsed.path
+    print(bitly_link)
+
+    change_to = 'bit.ly/correct1102021'
+    http_response = update_custom(headers, bitly_link, change_to)
+    print(http_response)
