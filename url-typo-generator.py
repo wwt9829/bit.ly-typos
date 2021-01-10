10611a10b250213eca49cfd8829b115e73249444
Wyatt Tauber <wwt9829@g.rit.edu>
2021-01-10 16:31:42 -0600

    add functions to skip, double, reverse letters

--- url-typo-generator.py
+++ url-typo-generator.py
@@ -1,34 +1,45 @@
 from urllib.parse import urlparse
 
+# TODO: comments
+
 
 def skip_letter(keyword, list):
     """
-
-    :param keyword:
-    :param list:
-    :return:
+    Generates all keywords with a letter skipped
+    :param keyword: a keyword to generate typos for
+    :param list: the list of typos to append to
     """
-    return list
+    i = 0
+    while i < len(keyword):
+        new_word = keyword[0:i] + keyword[i+1:]
+        list.append(new_word)
+        i += 1
 
 
 def double_letter(keyword, list):
     """
-
-    :param keyword:
-    :param list:
-    :return:
+    Generates all keywords with a letter duplicated
+    :param keyword: a keyword to generate typos for
+    :param list: the list of typos to append to
     """
-    return list
+    i = 0
+    while i < len(keyword):
+        new_word = keyword[0:i+1] + keyword[i] + keyword[i + 1:]
+        list.append(new_word)
+        i += 1
 
 
 def reverse_letters(keyword, list):
     """
-
-    :param keyword:
-    :param list:
-    :return:
+    Generates all keywords with a letter reversed
+    :param keyword: a keyword to generate typos for
+    :param list: the list of typos to append to
     """
-    return list
+    i = 0
+    while i < len(keyword) - 1:
+        new_word = keyword[0:i] + keyword[i + 1] + keyword[i] + keyword[i + 2:]
+        list.append(new_word)
+        i += 1
 
 
 def missed_key(keyword, list):
@@ -36,9 +47,7 @@ def missed_key(keyword, list):
 
     :param keyword:
     :param list:
-    :return:
     """
-    return list
 
 
 def inserted_key(keyword, list):
@@ -46,22 +55,22 @@ def inserted_key(keyword, list):
 
     :param keyword:
     :param list:
-    :return:
     """
     return list
 
 
 if __name__ == "__main__":
-    original_bitly_link = input("Enter bit.ly short URL to create typos:")
-    parsed_original_bitly_link = urlparse(original_bitly_link)
+    #original_bitly_link = input("Enter bit.ly short URL to create typos:")
 
-    path = parsed_original_bitly_link.path
+    #path = original_bitly_link.split("/")[-1]
+    path = 'test123'
     typo_list = []
 
-    typo_list = skip_letter(path, typo_list)
-    typo_list = double_letter(path, typo_list)
-    typo_list = reverse_letters(path, typo_list)
-    typo_list = missed_key(path, typo_list)
-    typo_list = inserted_key(path, typo_list)
+    skip_letter(path, typo_list)
+    double_letter(path, typo_list)
+    reverse_letters(path, typo_list)
+    #typo_list = missed_key(path, typo_list)
+    #typo_list = inserted_key(path, typo_list)
 
-    print(typo_list)
+    for typo in typo_list:
+        print(typo)
