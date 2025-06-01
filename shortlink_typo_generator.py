0ac51fec95789fb24a9edf58aeb8a598597618a7
Wyatt Tauber <me@wyatttauber.com>
2025-05-31 23:59:20 -0400

    tinyurl api key processing and validation

--- shortlink_typo_generator.py
+++ shortlink_typo_generator.py
@@ -3,7 +3,8 @@ from http import HTTPStatus
 import os
 import sys
 from typo_list_generator import make_typos
-from bitly_shortlink_creator import create_short_url
+import bitly_shortlink_creator
+import tinyurl_shortlink_creator
 import validators
 
 def validate_api_file():
@@ -17,24 +18,43 @@ def validate_api_file():
         exit(1)
 
     # load the API keys
-    bitly_api_key = ""
+    api_keys = {}
 
-    with open("api_keys.txt", "r") as api_keys:
-        for line in api_keys:
+    #bitly_api_key = ""
+    #tinyurl_api_key = ""
+
+    with open("api_keys.txt", "r") as api_keys_file:
+        for line in api_keys_file:
+            # bitly api key in the file
             if "bitly:" in line:
                 try:
-                    bitly_api_key = line.strip().split()[1]
+                    #bitly_api_key = line.strip().split()[1]
+                    api_keys["bitly"] = line.strip().split()[1]
+                    continue
                 except IndexError:
                     print('file error: Bit.ly API key missing', file=sys.stderr)
                     exit(1)
+
+            # tinyurl api key in the file
+            if "tinyurl:" in line:
+                try:
+                    api_keys["tinyurl"] = line.strip().split()[1]
+                    continue
+                except IndexError:
+                    print('file error: TinyUrl API key missing', file=sys.stderr)
+                    exit(1)
+
+            # something else in the file
             else:
                 print('file error: no API keys identified', file=sys.stderr)
                 exit(1)
 
     # return the api key
-    return bitly_api_key
+    #return bitly_api_key, tinyurl_api_key
+    return api_keys
 
-def validate_id(bit_id):
+
+def validate_bitly_id(bit_id):
     """
     Determines if a given bit.ly ID is properly formatted
     :param bit_id: the bit.ly ID to be evaluated
@@ -44,7 +64,7 @@ def validate_id(bit_id):
     if '/' not in bit_id:
         return False
 
-    # must have 'bit.ly' and and alpha-numeric code
+    # must have 'bit.ly' and alpha-numeric code
     components = bit_id.split('/')
     if not components[0] == 'bit.ly':
         return False
@@ -55,7 +75,28 @@ def validate_id(bit_id):
     return True
 
 
-def validate(k, l, s):
+def validate_tinyurl_id(tiny_id):
+    """
+    Determines if a given TinyURL ID is properly formatted
+    :param tiny_id: the TinyURL ID to be evaluated
+    :return: ID validity, as a boolean
+    """
+    # must have a /
+    if '/' not in tiny_id:
+        return False
+
+    # must have 'tinyurl.com' and alpha-numeric code
+    components = tiny_id.split('/')
+    if not components[0] == 'tinyurl.com':
+        return False
+    if not components[1].isalnum():
+        return False
+
+    # then it is a valid TinyURL ID
+    return True
+
+
+def validate_bitly(k, l, s):
     """
     Validate the api key, the long URL, and the bit.ly ID short link URL, exiting if invalid
     :param k: API key
@@ -68,8 +109,26 @@ def validate(k, l, s):
     if not validators.url(l):
         print('URL error: invalid URL (include full path)', file=sys.stderr)
         exit(1)
-    if not validate_id(s):
+    if not validate_bitly_id(s):
         print('shortlink error: invalid bit.ly ID', file=sys.stderr)
+        exit(1)
+
+
+def validate_tinyurl(k, l, s):
+    """
+    Validate the api key, the long URL, and the bit.ly ID short link URL, exiting if invalid
+    :param k: API key
+    :param l: a URL
+    :param s: a bit.ly ID
+    """
+    if not k.isalnum() or not len(k) == 60:
+        print('URL error: invalid API key', file=sys.stderr)
+        exit(1)
+    if not validators.url(l):
+        print('URL error: invalid URL (include full path)', file=sys.stderr)
+        exit(1)
+    if not validate_tinyurl_id(s):
+        print('shortlink error: invalid TinyURL ID', file=sys.stderr)
         exit(1)
 
 
@@ -193,16 +252,17 @@ def select_options():
     return preview, options
 
 
-def append_bitly_url(typo_list):
+def append_shortlink_domain(typo_list, domain):
     """
     Create a list of bit.ly ID typos
     :param typo_list: a list of typos
-    :return: a list of bit.ly ID typos
+    :param domain: a domain to append to the typos
+    :return: a list of shortlink ID typos
     """
     url_list = []
 
     for typo in typo_list:
-        url = 'bit.ly/' + typo
+        url = domain + "/" + typo
         url_list.append(url)
 
     return url_list
@@ -215,10 +275,12 @@ def create_bitly_typos(key, bitly_link, redirect_url, options, debug, bypass):
     :param bitly_link: a bit.ly ID to generate typos for
     :param redirect_url: a URL to redirect the typo'd bit.ly IDs to
     :param options: a dictionary of options selected by the user
+    :param debug: a boolean indicating whether to preview the typos before generating them
+    :param bypass: a boolean indicating whether to bypass the API call confirmation prompt
     :return: a list of successfully-created bit.ly hyperlinks
     """
     # validate the api key, the long URL, and the bit.ly ID short link URL
-    validate(key, redirect_url, bitly_link)
+    validate_bitly(key, redirect_url, bitly_link)
 
     # maintain a list of successfully-generated bit.ly links
     success_list = []
@@ -226,11 +288,11 @@ def create_bitly_typos(key, bitly_link, redirect_url, options, debug, bypass):
     # create a list of bit.ly ID typos
     path = bitly_link.split('/')[1]
     typos = make_typos(path, options, debug)
-    bitly_typos = append_bitly_url(typos)
+    bitly_typos = append_shortlink_domain(typos, "bit.ly")
 
     # confirm the number of typos to generate with the user if not bypassed
     if not bypass:
-        print("This will use", len(bitly_typos), "API calls. Press ENTER to confirm or any other key to exit... (-B to bypass this warning in cmd) ")
+        print("This will use", len(bitly_typos), "Bit.ly API calls. Press ENTER to confirm or any other key to exit... (-B to bypass this warning in cmd) ")
         if input() != "":
             exit(0)
 
@@ -238,7 +300,7 @@ def create_bitly_typos(key, bitly_link, redirect_url, options, debug, bypass):
     print('Attempting to create', len(bitly_typos), 'bit.ly typos...')
     for bitly_typo in bitly_typos:
         print('Creating', bitly_typo)
-        result = create_short_url(key, redirect_url, bitly_typo)
+        result = bitly_shortlink_creator.create_short_url(key, redirect_url, bitly_typo)
 
         # add successful domains to a list
         if result.status_code == HTTPStatus.OK:
@@ -248,6 +310,50 @@ def create_bitly_typos(key, bitly_link, redirect_url, options, debug, bypass):
     # return the list of successfully-generated bit.ly links
     return success_list
 
+
+def create_tinyurl_typos(key, tinyurl_link, redirect_url, options, debug, bypass):
+    """
+    Create a list of typos for a tinyurl link and register them with a URL
+    :param key: a tinyurl API key
+    :param tinyurl_link: a tinyurl ID to generate typos for
+    :param redirect_url: a URL to redirect the typo'd tinyurl IDs to
+    :param options: a dictionary of options selected by the user
+    :param debug: a boolean indicating whether to preview the typos before generating them
+    :param bypass: a boolean indicating whether to bypass the API call confirmation prompt
+    :return: a list of successfully-created tinyurl hyperlinks
+    """
+    # validate the api key, the long URL, and the TinyURL ID short link URL
+    validate_tinyurl(key, redirect_url, tinyurl_link)
+
+    # maintain a list of successfully-generated tinyurl links
+    success_list = []
+
+    # create a list of tinyurl ID typos
+    path = tinyurl_link.split('/')[1]
+    typos = make_typos(path, options, debug)
+    tinyurl_typos = append_shortlink_domain(typos, "tinyurl.com")
+
+    # confirm the number of typos to generate with the user if not bypassed
+    if not bypass:
+        print("This will use", len(tinyurl_typos), "TinyURL API calls. Press ENTER to confirm or any other key to exit... (-B to bypass this warning in cmd) ")
+        if input() != "":
+            exit(0)
+
+    # link the typo'd bit.ly IDs to a long URL
+    print('Attempting to create', len(tinyurl_typos), 'TinyURL typos...')
+    for tinyurl_typo in tinyurl_typos:
+        print('Creating', tinyurl_typo)
+        result = tinyurl_shortlink_creator.create_short_url(key, redirect_url, tinyurl_typo)  # TODO
+
+        # add successful domains to a list  # TODO
+        if result.status_code == HTTPStatus.OK:
+            new_link = 'https://' + tinyurl_typo
+            success_list.append(new_link)
+
+    # return the list of successfully-generated TinyURL links
+    return success_list
+
+
 if __name__ == '__main__':
     # print the welcome message
     print("############################")
@@ -256,7 +362,8 @@ if __name__ == '__main__':
     print("Generate and register common typos for your shortlinks! | by Wyatt Tauber (wyatttauber.com)")
 
     # read the API keys
-    bitly_api_key = validate_api_file()
+    #bitly_api_key, tinyurl_api_key = validate_api_file()
+    api_keys = validate_api_file()
 
     # initialize values to be assigned
     shortlink = ""
@@ -280,7 +387,7 @@ if __name__ == '__main__':
         print("cmd usage: shortlink_typo_generator.py [-h --help] [-s --skip] [-d --double] [-r --reverse] [-m --miss] [-c --case] [-A --all] [-P --preview] [-B --bypass (cmd only)] shortlink redirect_url\n")
 
         # get the shortlink ID and redirect URL from the user
-        shortlink = input('Enter a shortlink (bit.ly) to generate typos for: ').strip()
+        shortlink = input('Enter a shortlink (bit.ly or tinyurl.com) to generate typos for: ').strip()
         redirect = input('Enter a URL to redirect the typos to: ').strip()
         print()
 
@@ -290,8 +397,10 @@ if __name__ == '__main__':
     # create the links
     links = []
 
-    if "bit.ly" in shortlink:
-        links = create_bitly_typos(bitly_api_key, shortlink, redirect, options, debug, bypass)
+    if "bit.ly/" in shortlink:
+        links = create_bitly_typos(api_keys["bitly"], shortlink, redirect, options, debug, bypass)
+    elif "tinyurl.com/" in shortlink:
+        links = create_tinyurl_typos(api_keys["tinyurl"], shortlink, redirect, options, debug, bypass)
     else:
         print('shortlink error: shortlink provided is not a supported shortlink', file=sys.stderr)
         exit(1)
